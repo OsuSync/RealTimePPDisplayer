561ec74238c76cb8106a526801dd85316708e649
KedamaOvO <moe@mao-yu.net>
2018-02-28 18:33:01 +0800

    config-gui

--- DefaultLanguage.cs
+++ DefaultLanguage.cs
@@ -1,4 +1,5 @@
 ﻿using Sync.Tools;
+using Sync.Tools.ConfigGUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,62 @@ namespace RealTimePPDisplayer
         public static LanguageElement UI_MENU_TOPMOST = "Topmost";
         public static LanguageElement TEXT_MODE_OUTPUT_PATH_FORMAT = "[RealTimePPDisplayer]PP File: {0}";
         public static LanguageElement MMF_MODE_OUTPUT_PATH_FORMAT = "[RealTimePPDisplayer]Memory Mapping File: {0}";
+
+        [ConfigI18n]
+        public static LanguageElement FPS = "FPS";
+
+        [ConfigI18n]
+        public static LanguageElement TextOutputPath = "Text output path";
+
+        [ConfigI18n]
+        public static LanguageElement DisplayHitObject = "Display hit object";
+
+        [ConfigI18n]
+        public static LanguageElement FontName = "Font";
+
+        [ConfigI18n]
+        public static LanguageElement PPFontSize = "PP font size";
+
+        [ConfigI18n]
+        public static LanguageElement PPFontColor = "PP font color";
+
+        [ConfigI18n]
+        public static LanguageElement HitCountFontSize = "Hit count font size";
+
+        [ConfigI18n]
+        public static LanguageElement HitCountFontColor = "Hit count font color";
+
+        [ConfigI18n]
+        public static LanguageElement BackgroundColor = "Background color";
+
+        [ConfigI18n]
+        public static LanguageElement WindowHeight = "Window height";
+
+        [ConfigI18n]
+        public static LanguageElement WindowWidth = "Window width";
+
+        [ConfigI18n]
+        public static LanguageElement SmoothTime = "Smooth time";
+
+        [ConfigI18n]
+        public static LanguageElement Topmost = "Topmost";
+
+        [ConfigI18n]
+        public static LanguageElement WindowTextShadow = "Window text shadow";
+
+        [ConfigI18n]
+        public static LanguageElement OutputMethods = "Output methods";
+
+        [ConfigI18n]
+        public static LanguageElement DebugMode = "Debug mode";
+
+        [ConfigI18n]
+        public static LanguageElement PPFormat = "PP format";
+
+        [ConfigI18n]
+        public static LanguageElement HitCountFormat = "Hit count format";
+
+        [ConfigI18n]
+        public static LanguageElement RoundDigits = "Digits";
     }
 }
