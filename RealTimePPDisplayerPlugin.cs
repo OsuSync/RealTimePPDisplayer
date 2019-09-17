69069285a3b43132f71db71bc7a311f6f2cbe4a0
KedamaOvO <moe@mao-yu.net>
2019-09-17 15:44:03 +0800

    v1.8.5

--- RealTimePPDisplayerPlugin.cs
+++ RealTimePPDisplayerPlugin.cs
@@ -19,7 +19,7 @@ namespace RealTimePPDisplayer
     {
         public const string PLUGIN_NAME = "RealTimePPDisplayer";
         public const string PLUGIN_AUTHOR = "KedamaOvO";
-        public const string VERSION= "1.8.4";
+        public const string VERSION= "1.8.5";
 
         private readonly DisplayerController[] _osuPpControls = new DisplayerController[16];
 
